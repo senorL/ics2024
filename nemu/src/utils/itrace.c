599edb43dc702068ad4a17267a58260b288658e4
senorl <2870510275@qq.com>
2024-11-25 16:21:27 +0800

    fix: iringbuf

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -11,19 +11,28 @@ void init_iring() {
 }
 
 void add_iring(Decode *s) {
-    snprintf(iringbuf[buf_idex], 128, "0x%08x", s->pc);
-
+    char *p = iringbuf[buf_idex];
+    p += snprintf(p, 128, FMT_WORD ":", s->pc);
+    int ilen = s->snpc - s->pc;
+    uint8_t *inst = (uint8_t *)&s->isa.inst;
+    for (int i = ilen - 1; i >= 0; i --) {
+        p += snprintf(p, 4, " %02x", inst[i]);
+    }
+    int size = 128 - (p - iringbuf[buf_idex]);
+    void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+    disassemble(p, size, MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
     buf_idex = (buf_idex + 1) % IRINGBUF_NUM;
 }
 
 void iring_trace() {
-    for (int i = 0; i < buf_idex; i++) {
-        printf("%s\n", iringbuf[i]);
+    if (buf_idex == 0) return;
+    for (int i = 0; i < buf_idex - 1; i++) {
+        printf("    %s\n", iringbuf[i]);
     }
-    printf("--> %s\n", iringbuf[buf_idex - 1]);
+    printf("\033[1;31m--> %s\033[0m\n", iringbuf[buf_idex - 1]);
 
     for (int i = buf_idex; i < IRINGBUF_NUM; i++) {
-        printf("%s\n", iringbuf[i]);
+        printf("    %s\n", iringbuf[i]);
     }
 }
 

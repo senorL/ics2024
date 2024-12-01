fa290502d2ae49c01606a68747efa581dc4b50d5
senorl <2870510275@qq.com>
2024-12-01 15:32:16 +0800

    Add am-kernels as part of the main repository

---
+++ am-kernels/tests/klib-tests/write_functions.c
@@ -0,0 +1,44 @@
+#include <am.h>
+#include <klib-macros.h>
+#include <klib.h>
+
+#define N 32
+uint8_t data[N];
+
+void reset() {
+    for (int i = 0; i < N; i++) {
+        data[i] = i + 1;
+    }
+}
+void check_seq(int l, int r, int val) {
+  int i;
+  for (i = l; i < r; i ++) {
+    assert(data[i] == val + i - l);
+  }
+}
+
+void check_eq(int l, int r, int val) {
+  int i;
+  for (i = l; i < r; i ++) {
+    assert(data[i] == val);
+  }
+}
+
+void test_memset() {
+  int l, r;
+  for (l = 0; l < N; l ++) {
+    for (r = l + 1; r <= N; r ++) {
+      reset();
+      uint8_t val = (l + r) / 2;
+      memset(data + l, val, r - l);
+      check_seq(0, l, 1);
+      check_eq(l, r, val);
+      check_seq(r, N, r + 1);
+    }
+  }
+}
+
+int main() {
+    test_memset();
+    return 0;
+}
